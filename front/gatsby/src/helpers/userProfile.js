c623538f2af8dae28d68e43d2fc11b5fb96c547f
Thomas Parisot <oncletom@users.noreply.github.com>
2021-12-03 07:43:20 +0100

    Create a `useProfile()` hook, to refresh the profile at any time

--- front/gatsby/src/helpers/userProfile.js
+++ front/gatsby/src/helpers/userProfile.js
@@ -1,7 +1,19 @@
-export function getUserProfile(applicationConfig) {
-  return fetch(`${applicationConfig.backendEndpoint}/profile`, {
+import { useDispatch, useSelector } from "react-redux"
+
+export function getUserProfile({ backendEndpoint }) {
+  return fetch(`${backendEndpoint}/profile`, {
     method: 'GET',
     credentials: 'include',
     cors: true,
   }).then((res) => res.json())
+}
+
+export function useProfile () {
+  const backendEndpoint = useSelector(state => state.applicationConfig.backendEndpoint)
+  const dispatch = useDispatch()
+
+  return function refreshProfile () {
+    return getUserProfile({ backendEndpoint })
+      .then((response) => dispatch({ type: 'PROFILE', ...response }))
+  }
 }
